c42161f868dbf5a779b73a727ea29046fa9682d2
Robert Mulholand <robmulholand@gmail.com>
2014-07-11 10:00:00 -0400

    Fixed errors, improved naming

--- first_attempt/mastermind_game.rb
+++ first_attempt/mastermind_game.rb
@@ -1,12 +1,14 @@
 class MastermindGame
 
+# This program will have trouble with a dishonest user - it relies on accurate information to accurately calculate remaining possibilities
+
 	def initialize
 		@colors = 0
 		@rows = 0
 		@possible_colors = {red: 1, blue: 2, green: 3, yellow: 4, purple: 5, orange: 6, violet: 7, indigo: 8}
 		@double_check = ""
-		@black = ""
-		@white = ""
+		@correct_color_and_correct_placement = ""
+		@correct_color_and_incorrect_placement = ""
 	end
 
 	def play_game
@@ -25,28 +27,42 @@ class MastermindGame
 
 	def let_user_select_the_number_of_colors
 		puts "You can choose the number of possible colors available to our sequence." 
+		user_color_selection
+		@color_answer = ""
+	end
+
+	def user_color_selection
 		until @colors >= 2 && @colors <= 8 && (@color_answer[0] == "y" || @color_answer[0] == "Y")
 			puts "How many colors would you like to play with? (Pick a number between 2 and 8)"
 			@colors = gets.chomp.to_i
 			if @colors >= 2 && @colors <= 8
 				puts "OK, great. Is it correct that you want to play with #{@colors} colors? (y/n)"
 				@color_answer = gets.chomp
+			elsif @colors < 2 || @colors > 8
+				error_message
+				user_color_selection
 			end
 		end
-		@color_answer = ""
 	end
 
 	def let_user_select_the_number_of_rows
 		puts "You can also choose how long our sequence will be." 
+		user_row_selection
+		@rows_answer = ""
+	end
+
+	def user_row_selection
 		until @rows >= 2 && @rows <= 6 && (@rows_answer[0] == "y" || @rows_answer[0] == "Y")
 			puts "How long of a sequence do you want to play with? (Pick a number between 2 and 6)"
 			@rows = gets.chomp.to_i
-				if @rows >= 2 && @rows <= 6
-					puts "Sounds good. So you want to play with #{@rows} rows? (y/n)"
-					@rows_answer = gets.chomp
-				end
+			if @rows >= 2 && @rows <= 6
+				puts "Sounds good. So you want to play with #{@rows} rows? (y/n)"
+				@rows_answer = gets.chomp
+			elsif @rows < 2 || @rows > 6
+				error_message
+				user_row_selection
+			end
 		end
-		@rows_answer = ""
 	end
 
 	def game_setup
@@ -80,34 +96,34 @@ class MastermindGame
 
 	def provide_first_guess_to_the_user
 		@guess = @possible_combinations[rand(@possible_combinations.length)]
-		color_number_conversion
+		color_number_conversion(@guess)
 		puts "My first guess is " + @guess.to_s
 		@turns = 1
 		get_feedback_on_guess_from_user
 	end
 
 	def figure_out_the_secret_code
-		until @black.to_i == @rows
-			color_number_conversion
-			@possible_combinations.keep_if do |x|
-				compare_guess_to_remaining_options(@guess, x) == [@black.to_i, @white.to_i]
+		until @correct_color_and_correct_placement.to_i == @rows
+			color_number_conversion(@guess)
+			@possible_combinations.keep_if do |possible_combination|
+				compare_guess_to_remaining_options(@guess, possible_combination) == [@correct_color_and_correct_placement.to_i, @correct_color_and_incorrect_placement.to_i]
 			end
 			@guess = @possible_combinations[rand(@possible_combinations.length)]
-			color_number_conversion
+			color_number_conversion(@guess)
 			puts "OK, my next guess is " + @guess.to_s
 			@turns += 1
 			get_feedback_on_guess_from_user
 		end
 	end
 
-	def color_number_conversion
-		if @guess[0].class == Fixnum
-			@guess.each_with_index do |value, index|
-				@guess[index] = @possible_colors.key(value).to_s
+	def color_number_conversion(guess)
+		if guess[0].class == Fixnum
+			guess.each_with_index do |value, index|
+				guess[index] = @possible_colors.key(value).to_s
 			end
-		elsif @guess[0].class == String
-			@guess.each_with_index do |value, index|
-				@guess[index] = @possible_colors[value.to_sym]
+		elsif guess[0].class == String
+			guess.each_with_index do |value, index|
+				guess[index] = @possible_colors[value.to_sym]
 			end
 		end
 	end
@@ -136,21 +152,49 @@ class MastermindGame
 	end
 
 	def get_feedback_on_guess_from_user
-		until @black.to_i >= 0 && @black.to_i <= @rows && @white.to_i >= 0 && @white.to_i <= @rows && (@double_check[0] == "y" || @double_check[0] == "Y")
-			puts "How many of my picks are the correct color AND in the correct position?"
-			@black = gets.chomp
-				if @black.to_i == @rows
-					puts "I figured out your code in #{@turns} turns!"
-					break
-				end
-			puts "How many of my picks are the correct color in the incorrect position?"
-			@white = gets.chomp
-			puts "OK, so I have #{@black} picks with the correct color in the correct position, and #{@white} picks with the correct color in the incorrect position? (y/n)"
+		until @double_check[0] == "y" || @double_check[0] == "Y"
+			get_feedback_from_user_on_correct_color_and_correct_placement
+			get_feedback_from_user_on_correct_color_and_incorrect_placement
+			if @correct_color_and_correct_placement.to_i == 3 && @correct_color_and_incorrect_placement.to_i == 1
+				error_message
+				get_feedback_on_guess_from_user
+			end
+			puts "OK, so I have #{@correct_color_and_correct_placement} picks with the correct color in the correct position, and #{@correct_color_and_incorrect_placement} picks with the correct color in the incorrect position? (y/n)"
 			@double_check = gets.chomp
 		end
 		@double_check = ""
 	end
 
+# It'd be nice to break up the above so that a bad response to the double check (i.e. something that's not "no" doesn't auto-resend to re-selecting number of correct/close)
+
+	def get_feedback_from_user_on_correct_color_and_correct_placement
+		puts "How many of my picks are in the correct color and in the correct position? (0-4)"
+		@correct_color_and_correct_placement = gets.chomp
+		if @correct_color_and_correct_placement.to_i < 0 || @correct_color_and_correct_placement.to_i > @rows
+			error_message
+			get_feedback_from_user_on_correct_color_and_correct_placement
+		elsif @correct_color_and_correct_placement.to_i == 0 && @correct_color_and_correct_placement.strip != "0"
+			error_message
+			get_feedback_from_user_on_correct_color_and_correct_placement
+		end
+		abort("I figured out your code in #{@turns} turns!") unless @correct_color_and_correct_placement.to_i != @rows
+	end
+
+	def get_feedback_from_user_on_correct_color_and_incorrect_placement
+		puts "How many of my picks are the correct color but in the incorrect placement? (0-4)"
+		@correct_color_and_incorrect_placement = gets.chomp
+		if @correct_color_and_incorrect_placement.to_i < 0 || @correct_color_and_incorrect_placement.to_i > @rows
+			error_message
+			get_feedback_from_user_on_correct_color_and_incorrect_placement
+		elsif @correct_color_and_incorrect_placement.to_i == 0 && @correct_color_and_incorrect_placement.strip != "0"
+			error_message
+			get_feedback_from_user_on_correct_color_and_incorrect_placement
+		end
+	end
+
+	def error_message
+		puts "Oops! That's not a valid response. Try again."
+	end
 
 end
 
