f825e4bfe30f2a3ab792e42985fc0cb713b5ce36
Robert Mulholand <robmulholand@gmail.com>
2014-09-17 09:42:15 -0500

    early and often

--- second_attempt/lib/game_ai.rb
+++ second_attempt/lib/game_ai.rb
@@ -1,20 +1,29 @@
 class GameAI
 
-  attr_accessor :number_of_colors, :number_of_rows
+  attr_accessor :number_of_colors, :number_of_rows, :possible_combinations
 
   def initialize(number_of_colors, number_of_rows)
     self.number_of_colors = number_of_colors 
     self.number_of_rows = number_of_rows
+    self.possible_combinations = generate_all_combinations
   end
 
   def generate_all_combinations	
-    colors_array = convert_colors_number_to_colors_array(number_of_colors)  
-		all_possible_combinations = colors_array.repeated_permutation(number_of_rows).to_a
-		all_possible_combinations
+    colors_array = convert_colors_number_to_colors_array  
+		all_possible_combinations = colors_array.repeated_permutation(number_of_rows)
+		all_possible_combinations.to_a
   end
 
-  def convert_colors_number_to_colors_array(number)
-    array_of_colors = (1..number).to_a
+  def convert_colors_number_to_colors_array
+    (1..number_of_colors).to_a
   end
+
+  def generate_guess
+    possible_combinations[rand(generate_all_combinations.length)]
+  end
+
+  def compare_guesses(first_guess, second_guess)
+    [1, 2]
+	end
 
 end
--- second_attempt/spec/game_ai_spec.rb
+++ second_attempt/spec/game_ai_spec.rb
@@ -3,8 +3,30 @@ require 'game_ai'
 describe GameAI do
   let(:test) {described_class.new(6, 4)}
 
-  it "generates all possible combinations" do
-    expect(test.generate_all_combinations.length).to eq(1296)
+  it "generates the proper number of combinations" do
+    possible_combinations = test.generate_all_combinations
+    expect(possible_combinations.length).to eq(1296)
   end
+
+	it "generates the right combinations" do
+		possible_combinations = test.generate_all_combinations
+		expect(possible_combinations).to include([1, 1, 1, 1], [1, 1, 2, 2], [1, 2, 3, 4], [3, 4, 5, 6], [6, 6, 6, 6])
+  end
+
+  it "converts integers to arrays" do
+    expect(test.convert_colors_number_to_colors_array).to eq([1, 2, 3, 4, 5, 6])
+  end
+
+	it "generates a guess" do
+    my_guess = test.generate_guess
+    expect(my_guess.length).to eq(4)
+	end
+
+	it "compares two guesses" do
+    my_first_guess = test.generate_guess
+		my_second_guess = test.generate_guess
+		my_comparison = test.compare_guesses(my_first_guess, my_second_guess)
+		expect(my_comparison.length).to eq(2)
+	end
 
 end
